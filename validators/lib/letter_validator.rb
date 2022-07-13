3d7d0debcca9e5966d29d0ef1a059b46868584f8
Rahul <rahulpargaonkar1@gmail.com>
2022-07-13 14:09:48 +0530

    added implementation for validate method for both spec

--- validators/lib/letter_validator.rb
+++ validators/lib/letter_validator.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 class LetterValidator
+
+  attr_reader :letter
+
+  def letter_class
+    return letter.match(/[aeiou]/) ? :vowel : :consonant
+  end
+
+  def letter_half
+    return letter.match(/[a-m]/) ? :first_half : :second_half
+  end
+
+  def most_comman_letter
+    return :common_letter if letter.match(/[etaio]/)
+  end
+
   def validate(letter)
     # WRITE THIS CODE
+    @letter=letter
+    raise InvalidLetterError if letter.length > 1
+    [] << letter_class << letter_half << most_comman_letter
   end
 end
 
--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 
 class NumberValidator
+
+  attr_reader :number
+
+  def number_groups
+    return number.even? ? :even : :odd
+  end
+
+  def divisible_by_three
+    return __method__ if number.modulo(3).zero?
+  end
+
+  def divisible_by_five
+    return __method__ if number.modulo(5).zero?
+  end
+
+  def divisible_by_seven
+    return __method__ if number.modulo(7).zero?
+  end
+
+  def divisible_by_nine
+    return __method__ if number.modulo(9).zero?
+  end
+
   def validate(number)
     # WRITE THIS CODE
+    @number=number
+    raise InvalidNumberError unless number.is_a?(Integer)
+    [] << number_groups  << divisible_by_three << divisible_by_five <<
+      divisible_by_seven << divisible_by_nine
+
   end
 end
 
