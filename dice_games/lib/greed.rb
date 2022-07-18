d606c809baa8960a9f5b401321a13d6524cefd63
Rahul <rahulpargaonkar1@gmail.com>
2022-07-18 14:04:39 +0530

    commited changes for single input

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -2,9 +2,18 @@
 
 class Greed
 
+  attr_reader= :single_score_hash
+
+  def initialize
+    @single_score_hash= {  1 =>100 ,2=> 0, 3 => 0 , 4 => 0 , 5 =>50, 6 => 0  }
+  end
+
   def score(num=nil)
     raise GreedError unless num.is_a? Array
     return 0 if num==[]
+
+    return @single_score_hash[num[0]] if num.length==1
+
   end
 end
 
