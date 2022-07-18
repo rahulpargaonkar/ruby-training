e419438ebbba6d6e44c85aac4624dd2cde09db50
Rahul <rahulpargaonkar1@gmail.com>
2022-07-18 18:59:43 +0530

    added implemetation for low score hand by modifiend existing statement

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -1,39 +1,32 @@
 # frozen_string_literal: true
 
 class Greed
-
   attr_reader :single_score_hash, :tripple_score_hash
 
   def initialize
-    @single_score_hash= {  1 =>100 ,2=> 0, 3 => 0 , 4 => 0 , 5 =>50, 6 => 0  }
-    @tripple_score_hash= {  1 =>1000 ,2=> 100, 3 => 100 , 4 => 100 , 5 =>100, 6 => 100  }
+    @single_score_hash = { 1 => 100, 2 => 0, 3 => 0, 4 => 0, 5 => 50, 6 => 0 }
+    @triple_score_hash = {  1 => 1000, 2 => 100, 3 => 100, 4 => 100, 5 => 100, 6 => 100 }
   end
 
-
-
-  def score(num=nil)
+  def score(num = nil)
     raise GreedError unless num.is_a? Array
-    return 0 if num==[]
+    return 0 if num == []
 
-    return @single_score_hash[num[0]] if num.length==1
+    return @single_score_hash[num[0]] if num.length == 1
 
-    return num[0]*@tripple_score_hash[num[0]] if num.length==3
+    return num[0] * @triple_score_hash[num[0]] if num.length == 3
 
-    return score_for_specific_input num if num==[1,5,5,1] || [2,3,4,6]
-
-
-
+    return score_for_specific_input num if num == [1, 5, 5, 1] || num == [2, 3, 4, 6] || num.uniq == num
   end
 
   private
 
   def score_for_specific_input(num)
-    total=0
-    num.each{|item| total=total+@single_score_hash[item]}
+    total = 0
+    puts num.to_s
+    num.each { |item| total += @single_score_hash[item] }
     total
   end
-
 end
 
 class GreedError < ArgumentError; end
-
