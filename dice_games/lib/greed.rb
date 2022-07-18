8cc8bd042bb38bcf4116d7f427cf79da79495061
Rahul <rahulpargaonkar1@gmail.com>
2022-07-18 13:41:18 +0530

    added implementation when array is blank

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -2,9 +2,11 @@
 
 class Greed
 
-  def score
-    raise GreedError
+  def score(num=nil)
+    raise GreedError unless num.is_a? Array
+    return 0 if num==[]
   end
 end
 
 class GreedError < ArgumentError; end
+
