1e0b6f9bbbce925c339c172f3e1650db947d8bd9
Rahul <rahulpargaonkar1@gmail.com>
2022-07-18 12:29:04 +0530

    added implemetation for method roll

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Dice
-  def roll
+  attr_reader :values
 
+  def roll(number = 1)
+    @values = []
+    while number >= 1
+      @values << rand(1..6)
+      number -= 1
+    end
+    @values
   end
 end
