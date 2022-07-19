68a554160b90b195bcd1f20cbd745086f9b42763
Rahul <rahulpargaonkar1@gmail.com>
2022-07-19 12:35:49 +0530

    Added implementation for scenario high end [1,1,1,1,1]

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -8,6 +8,8 @@ class Greed
     @triple_score_hash = {  1 => 1000, 2 => 100, 3 => 100, 4 => 100, 5 => 100, 6 => 100 }
   end
 
+
+
   def score(num = nil)
     raise GreedError unless num.is_a? Array
     return 0 if num == []
@@ -17,6 +19,8 @@ class Greed
     return num[0] * @triple_score_hash[num[0]] if num.length == 3
 
     return score_for_specific_input num if num == [1, 5, 5, 1] || num == [2, 3, 4, 6] || num.uniq == num
+
+    return high_score(num)
   end
 
   private
@@ -25,6 +29,24 @@ class Greed
     total = 0
     puts num.to_s
     num.each { |item| total += @single_score_hash[item] }
+    total
+  end
+
+  def high_score(num)
+    num_hash=num.tally
+    total=0
+    num_hash.map do
+    |item,occurrence|
+      while occurrence>0
+        if occurrence>=3
+          total=total+(item*@triple_score_hash[item])
+          occurrence=occurrence-3
+        else
+          total=total+(item*@single_score_hash[item])
+          occurrence=occurrence-1
+        end
+      end
+    end
     total
   end
 end
